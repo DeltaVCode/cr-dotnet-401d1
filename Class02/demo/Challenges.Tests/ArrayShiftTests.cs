be52b9bd72bbdb506d725c79657e37e8a9f32f17
Keith Dahlby <keith@newbo.co>
2020-05-06 10:22:00 -0500

    Implement Challenge 2, TDD

--- Class02/demo/Challenges.Tests/ArrayShiftTests.cs
+++ Class02/demo/Challenges.Tests/ArrayShiftTests.cs
@@ -5,17 +5,55 @@ namespace Challenges.Tests
     public class ArrayShiftTests
     {
         [Fact]
-        public void Can_shit_empty_array()
+        public void Can_shift_into_even_length()
         {
             // Arrange
-            int[] input = new int[0];
+            int[] input = new[] { 1, 2, 4, 5 };
 
             // Act
-            int[] result = ArrayChallenges.ArrayShift(input);
+            int[] result = ArrayChallenges.ArrayShift(input, 3);
 
             // Assert
-            // TODO
-            Assert.NotNull(result);
+            Assert.Equal(new[] { 1, 2, 3, 4, 5 }, result);
+        }
+
+        [Fact]
+        public void Can_shift_into_odd_length()
+        {
+            // Arrange
+            int[] input = new[] { 1, 2, 3, 5, 6 };
+
+            // Act
+            int[] result = ArrayChallenges.ArrayShift(input, 4);
+
+            // Assert
+            Assert.Equal(new[] { 1, 2, 3, 4, 5, 6 }, result);
+        }
+
+        [Theory]
+        [InlineData(1, new int[0], new[] { 1 })] // Even (Zero)
+        [InlineData(2, new int[0], new[] { 2 })]
+        [InlineData(3, new int[] { 2 }, new[] { 2, 3 })] // Odd (1)
+        [InlineData(3, new int[] { 2, 4 }, new[] { 2, 3, 4 })] // Even (2)
+        [InlineData(4, new int[] { 1, 2, 3, 5, 6 }, new[] { 1, 2, 3, 4, 5, 6 })] // Odd (5)
+        public void Can_shift(int value, int[] input, int[] expected)
+        {
+            // Arrange
+            // from parameters
+
+            // Act
+            int[] result = ArrayChallenges.ArrayShift(input, value);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Returns_null_given_null()
+        {
+            int[] result = ArrayChallenges.ArrayShift(null, 1);
+
+            Assert.Null(result);
         }
     }
 }
--- Class02/demo/Challenges/ArrayChallenges.cs
+++ Class02/demo/Challenges/ArrayChallenges.cs
@@ -10,9 +10,42 @@ namespace Challenges
             return array.Reverse().ToArray();
         }
 
-        public static int[] ArrayShift(int[] array)
+        public static int[] ArrayShift(int[] array, int valueToInsert)
         {
-            return array; // TODO
+            if (array == null) return null;
+
+            // Wrong:
+            // decimal midPoint = array.Length / 2.0m;
+            // int midPoint = array.Length / 2;
+
+            // Right:
+            // decimal midPoint = Math.Ceiling(array.Length / 2m);
+            int midPoint = (array.Length + 1) / 2;
+
+            int[] result = new int[array.Length + 1];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i < midPoint)
+                {
+                    result[i] = array[i];
+                }
+                else if (i == midPoint)
+                {
+                    result[i] = valueToInsert;
+                }
+                else
+                {
+                    result[i] = array[i - 1];
+                }
+            }
+
+            return result;
+        }
+
+        public static bool BinarySearch(int[] array)
+        {
+            return false;
         }
     }
 }
