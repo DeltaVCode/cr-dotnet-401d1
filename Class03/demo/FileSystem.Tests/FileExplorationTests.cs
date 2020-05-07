2f9d9cf7beab8a529ae6afa34f7fbef3ec111144
Keith Dahlby <keith@newbo.co>
2020-05-07 10:38:31 -0500

    Review Day 3

--- Class03/demo/FileSystem.Tests/FileExplorationTests.cs
+++ Class03/demo/FileSystem.Tests/FileExplorationTests.cs
@@ -77,15 +77,53 @@ namespace FileSystem.Tests
 
             File.WriteAllLines(path, initialContents);
 
-            string[] contentsToAppend = new[] { "Second line" };
+            string valueToAppend = "Second line";
 
             // Act
-            File.AppendAllLines(path, contentsToAppend);
+            Program.AppendSingleLine(path, valueToAppend);
 
             // Assert
             string[] expectedContents = new[] { "First line", "Second line" };
             string[] actualContents = File.ReadAllLines(path);
             Assert.Equal(expectedContents, actualContents);
+        }
+
+        [Fact]
+        public void Can_remove_line_from_file()
+        {
+            // Arrange
+            string path = "Assets/remove-from-me.txt";
+            File.WriteAllLines(path, new[] { "One", "Two", "Three" });
+
+            // Act
+            Program.RemoveLine(path, "Three");
+
+            // Assert
+            Assert.Equal(new[] { "One", "Two" }, File.ReadAllLines(path));
+        }
+
+        [Theory]
+        [InlineData(new string[0], "Hello", new string[0])]
+        [InlineData(new[] { "Hello" }, "Hello", new string[0])]
+        [InlineData(new[] { "1", "2" }, "1", new[] { "2" })]
+        [InlineData(new[] { "1", "2" }, "2", new[] { "1" })]
+        [InlineData(new[] { "1", "2", "3" }, "1", new[] { "2", "3" })]
+        [InlineData(new[] { "1", "2", "3" }, "2", new[] { "1", "3" })]
+        [InlineData(new[] { "1", "2", "3" }, "3", new[] { "1", "2" })]
+        [InlineData(new[] { "1", "2", "3" }, "0", new[] { "1", "2", "3" })]
+        [InlineData(new[] { "1", "2", "1" }, "1", new[] { "2" })]
+        [InlineData(new[] { "1", "1", "1" }, "1", new string[0])]
+        public void Can_remove_from_array(string[] array, string value, string[] expected)
+        {
+            // Arrange
+            // from parameters
+
+            // Act
+            string[] result = Program.RemoveItemFromArray(array, value);
+
+            // Assert
+            Assert.Equal(expected, result);
+            Assert.NotSame(array, result);
         }
     }
 }
--- Class03/demo/FileSystem/Program.cs
+++ Class03/demo/FileSystem/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.IO;
 
 namespace FileSystem
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static void AppendSingleLine(string path, string valueToAppend)
+        {
+            string[] contentsToAppend = new[] { valueToAppend };
+            File.AppendAllLines(path, contentsToAppend);
+        }
+
+        public static void RemoveLine(string path, string valueToRemove)
+        {
+            string[] existingLines = File.ReadAllLines(path);
+
+            string[] result = RemoveItemFromArray(existingLines, valueToRemove);
+
+            File.WriteAllLines(path, result);
+        }
+
+        public static string[] RemoveItemFromArray(string[] array, string valueToRemove)
+        {
+            int valueCount = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == valueToRemove)
+                {
+                    valueCount++;
+                }
+            }
+            // valueCount = number of times valueToRemove is in array
+
+            string[] result = new string[array.Length - valueCount];
+            for (int i = 0, j = 0; i < array.Length; i++)
+            {
+                if (array[i] != valueToRemove)
+                {
+                    result[j++] = array[i];
+                }
+            }
+            return result;
         }
     }
 }
