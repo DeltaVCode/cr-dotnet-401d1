2e291cd5308553ed4aab5a4b852396c4b7350dba
Keith Dahlby <keith@newbo.co>
2020-05-13 12:18:33 -0500

    Collections demo

---
+++ Class08/Demo/MyGenericList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    public class MyGenericList<T> : IEnumerable<T>
+    {
+        // Fields
+        T[] items;
+        int count;
+
+        // Constructors
+        public MyGenericList(int capacity)
+        {
+            if (capacity <= 0) throw new ArgumentException();
+
+            items = new T[capacity];
+        }
+
+        public MyGenericList() : this(5)
+        {
+        }
+
+        // Properties
+        public int Count => count;
+
+        public T this[int index] => items[index];
+
+        public void Add(T value)
+        {
+            // Ran out of space!
+            if (items.Length == count)
+            {
+                // Double the size of the array
+                Array.Resize(ref items, count * 2);
+            }
+
+            items[count] = value;
+            count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+        // Non-generic, just do this because we have to
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Class08/Demo/MyGenericListTests.cs
@@ -0,0 +1,81 @@
+﻿using Xunit;
+
+namespace Demo
+{
+    public class MyGenericListTests
+    {
+        [Fact]
+        public void Initial_list_is_empty()
+        {
+            // Arrange
+
+            // Act
+            MyGenericList<string> myList = new MyGenericList<string>();
+
+            // Assert
+            Assert.Equal(0, myList.Count);
+        }
+
+        [Fact]
+        public void Can_add_to_empty_list()
+        {
+            // Arrange
+            MyGenericList<int> myList = new MyGenericList<int>();
+
+            // Act
+            myList.Add(5);
+
+            // Assert
+            Assert.Equal(1, myList.Count);
+            Assert.Equal(5, myList[0]);
+        }
+
+        [Fact]
+        public void Can_add_to_list_beyond_initial_capacity()
+        {
+            // Arrange
+            const int Capacity = 1;
+            MyGenericList<int> myList = new MyGenericList<int>(Capacity);
+            myList.Add(5);
+
+            // Act
+            myList.Add(10);
+
+            // Assert
+            Assert.Equal(2, myList.Count);
+            Assert.Equal(5, myList[0]);
+            Assert.Equal(10, myList[1]);
+
+            // Act
+            myList.Add(20);
+            myList.Add(40);
+            myList.Add(80);
+
+            // Assert
+            Assert.Equal(5, myList.Count);
+            Assert.Equal(80, myList[4]);
+        }
+
+
+        [Fact]
+        public void Can_enumerate_a_list()
+        {
+            // Behind the scenes, just calls Add()
+            MyGenericList<string> myList = new MyGenericList<string>
+            {
+                "Keith",
+                "Craig",
+                "Ian",
+            };
+
+            foreach (string item in myList)
+            {
+                Assert.NotNull(item);
+            }
+
+            Assert.Equal(
+                new[] { "Keith", "Craig", "Ian" },
+                myList);
+        }
+    }
+}
