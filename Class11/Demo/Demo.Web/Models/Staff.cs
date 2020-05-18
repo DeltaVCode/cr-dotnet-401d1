ff671b3a2d919103625cca8c36c6dd97d778f9e2
Keith Dahlby <keith@newbo.co>
2020-05-18 12:13:23 -0500

    Let's parse some csv!

--- Class11/Demo/Demo.Web/Models/Staff.cs
+++ Class11/Demo/Demo.Web/Models/Staff.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Demo.Web.Models
 {
@@ -9,12 +12,23 @@ namespace Demo.Web.Models
 
         public static List<Staff> GetStaff()
         {
-            return new List<Staff>
-            {
-                new Staff { Name = "Keith", Role = Role.Instructor },
-                new Staff { Name = "Craig", Role = Role.Instructor },
-                new Staff { Name = "Aaron", Role = Role.Admin },
-            };
+            //return new List<Staff>
+            //{
+            //    new Staff { Name = "Keith", Role = Role.Instructor },
+            //    new Staff { Name = "Craig", Role = Role.Instructor },
+            //    new Staff { Name = "Aaron", Role = Role.Admin },
+            //};
+            string[] staff = File.ReadAllLines("App_Data/Staff.csv");
+
+            return staff
+                .Skip(1) // skip the header
+                .Select(staffLine => staffLine.Split(",")) // split line into cells
+                .Select(staffCells => new Staff // make new Staff from cells
+                {
+                    Name = staffCells[0],
+                    Role = (Role)Enum.Parse(typeof(Role), staffCells[1]),
+                })
+                .ToList();
         }
     }
 
