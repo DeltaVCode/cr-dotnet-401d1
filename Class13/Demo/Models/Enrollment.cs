8c3525fa16a3da90936af0c54f4e4349a9b4e2fc
Keith Dahlby <keith@newbo.co>
2020-05-20 11:52:54 -0500

    Add Student/Enrollment

---
+++ Class13/Demo/Models/Enrollment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models
+{
+    public class Enrollment
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public Student Student { get; set; }
+
+        [Required]
+        public Course Course { get; set; }
+    }
+}
---
+++ Class13/Demo/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
