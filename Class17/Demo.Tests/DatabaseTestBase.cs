e6203e623352d07e5d9e56936d256cc4c974aaec
Keith Dahlby <keith@newbo.co>
2020-05-27 21:50:20 -0500

    Add base class for SQLite tests

---
+++ Class17/Demo.Tests/DatabaseTestBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Demo.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Tests
+{
+    public abstract class DatabaseTestBase : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+        protected readonly SchoolDbContext _db;
+
+        public DatabaseTestBase()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            _db = new SchoolDbContext(
+                new DbContextOptionsBuilder<SchoolDbContext>()
+                    .UseSqlite((System.Data.Common.DbConnection)_connection)
+                    .Options);
+
+            _db.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _db?.Dispose();
+            _connection?.Dispose();
+        }
+    }
+}
