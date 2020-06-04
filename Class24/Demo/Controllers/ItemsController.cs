4bd0af52220889789bd83206b10f1fd989fe9a85
Keith Dahlby <keith@newbo.co>
2020-06-04 11:09:36 -0500

    Add Checkbox list demo

---
+++ Class24/Demo/Controllers/ItemsController.cs
@@ -0,0 +1,101 @@
+﻿using System.Linq;
+using Demo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            var model = new EditItemViewModel
+            {
+                ItemModifiers =
+                    StoreModifier.GetAll()
+                        .Select(sm => new EditItemModifier
+                        {
+                            Id = sm.Id,
+                            Name = sm.Name,
+                        })
+                        .ToList(),
+            };
+            return View(model);
+        }
+
+        // POST: Items/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EditItemViewModel data)
+        {
+            try
+            {
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Items/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Class24/Demo/Models/EditItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Demo.Models
+{
+    public class EditItemViewModel
+    {
+        public string Name { get; set; }
+
+        public List<EditItemModifier> ItemModifiers { get; set; }
+    }
+
+    public class StoreModifier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static StoreModifier[] GetAll() =>
+            new[]
+            {
+                new StoreModifier { Id = 1, Name = "Soy Milk" },
+                new StoreModifier { Id = 2, Name = "Extra Espresso" },
+            };
+    }
+
+    public class EditItemModifier
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Selected { get; set; }
+
+        public decimal? AdditionalCost { get; set; }
+    }
+}
