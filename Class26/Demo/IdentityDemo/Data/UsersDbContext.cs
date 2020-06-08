4150ef9db4892f734467947856cff9f3fde3a69d
Keith Dahlby <keith@newbo.co>
2020-06-08 14:01:48 -0500

    Seed roles

--- Class26/Demo/IdentityDemo/Data/UsersDbContext.cs
+++ Class26/Demo/IdentityDemo/Data/UsersDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityDemo.Models.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,16 @@ namespace IdentityDemo.Data
     {
         public UsersDbContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var admin = new IdentityRole { Id = "admin", Name = "Administrator" };
+            var moderator = new IdentityRole { Id = "moderator", Name = "Moderator" };
+            builder.Entity<IdentityRole>()
+                .HasData(admin, moderator);
         }
     }
 }
--- Class26/Demo/IdentityDemo/Migrations/UsersDb/20200608163915_SeedRoles.Designer.cs
+++ Class26/Demo/IdentityDemo/Migrations/UsersDb/20200608163915_SeedRoles.Designer.cs
@@ -4,14 +4,16 @@ using IdentityDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentityDemo.Migrations.UsersDb
 {
     [DbContext(typeof(UsersDbContext))]
-    partial class UsersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200608163915_SeedRoles")]
+    partial class SeedRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
