f7a8476cd43dac86fe4c50621e540e7e4d890479
Keith Dahlby <keith@newbo.co>
2020-06-09 10:26:41 -0500

    Seed an admin user

--- Class27/Demo/IdentityDemo/Data/UsersDbContext.cs
+++ Class27/Demo/IdentityDemo/Data/UsersDbContext.cs
@@ -19,6 +19,21 @@ namespace IdentityDemo.Data
             var moderator = new IdentityRole { Id = "moderator", Name = "Moderator" };
             builder.Entity<IdentityRole>()
                 .HasData(admin, moderator);
+
+            var h = new PasswordHasher<BlogUser>();
+            var user = new BlogUser
+            {
+                Id = "dbfcc1a5-fe40-4539-9e3c-ae5d75c09e19",
+                UserName = "keith",
+                FirstName = "Keith",
+                LastName = "Dahlby",
+            };
+            user.PasswordHash = h.HashPassword(user, "p@ssw0rd");
+            builder.Entity<BlogUser>()
+                .HasData(user);
+
+            builder.Entity<IdentityUserRole<string>>()
+                .HasData(new IdentityUserRole<string> { RoleId = "admin", UserId = user.Id });
         }
     }
 }
--- Class27/Demo/IdentityDemo/Migrations/UsersDb/20200609151759_SeedAdminUser.Designer.cs
+++ Class27/Demo/IdentityDemo/Migrations/UsersDb/20200609151759_SeedAdminUser.Designer.cs
@@ -4,14 +4,16 @@ using IdentityDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentityDemo.Migrations.UsersDb
 {
     [DbContext(typeof(UsersDbContext))]
-    partial class UsersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200609151759_SeedAdminUser")]
+    partial class SeedAdminUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
