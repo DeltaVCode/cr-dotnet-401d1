61d228bc71eb172abb31f7873f99bd692961e467
Keith Dahlby <keith@newbo.co>
2020-06-15 11:54:12 -0500

    Add word reversing

--- Class31/demo/react-demo/src/App.js
+++ Class31/demo/react-demo/src/App.js
@@ -12,17 +12,51 @@ function Header() {
 
 // Another function component
 const Footer = () => (
-  <div class="footer">
+  <div className="footer">
     &copy; 2020
   </div>
 );
 
 // Class components!
 class Main extends React.Component {
+  constructor(props) {
+    // Just do this, don't ask why yet
+    super(props);
+
+    // The only time you can assign this.state
+    this.state = {
+      words: 'Reverse me!',
+    };
+  }
+
+  // Event handler is a class property
+  handleWord = e => {
+    let words = e.target.value;
+
+    // Set words in our component state
+    this.setState({ words });
+    // DO NOT this.state = { words }
+    console.log('__STATE__', this.state);
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    let words = this.state.words
+      .split('').reverse().join('');
+
+    this.setState({ words })
+  }
+
   render() {
+    // Re-rendering is automatic when state changes
+    // console.log('Rendering!', this.state );
+
     return (
-      <div class="wrapper">
+      <div className="wrapper">
         <h2>Main!</h2>
+        <h3>Words: {this.state.words}</h3>
+        <input onChange={this.handleWord}/>
+        <button onClick={this.handleClick}>Reverse</button>
       </div>
     )
   }
