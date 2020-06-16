8d7451d368fd5e3e5ed195b112c1b5f70c43226b
Keith Dahlby <keith@newbo.co>
2020-06-16 10:41:52 -0500

    First render snapshot

--- Class32/demo/tdd/src/__tests__/components/counter.test.js
+++ Class32/demo/tdd/src/__tests__/components/counter.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Counter from '../../components/counter';
 
@@ -54,5 +55,13 @@ describe('<Counter />', () => {
     app.find('button').simulate('click');
 
     expect(app.find('h1').exists()).toBe(true);
+  })
+
+  // Snapshot test!
+  it('renders correctly', () => {
+    let tree = renderer.create(<Counter />).toJSON();
+    //console.log(tree);
+
+    expect(tree).toMatchSnapshot();
   })
 })
