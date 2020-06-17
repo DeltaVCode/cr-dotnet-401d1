0c822ea61941f894c2ea780801501d8751e58677
Keith Dahlby <keith@newbo.co>
2020-06-17 11:05:00 -0500

    Add our NumberForm

--- Class33/demo/props/src/App.js
+++ Class33/demo/props/src/App.js
@@ -10,10 +10,25 @@ class App extends React.Component {
     }
   }
 
+  updateNumber = num => {
+    console.log('New Number', num);
+    this.setState({ number: num });
+    console.log('App State', this.state);
+  }
+
+  logAll = whatever => {
+    console.log('LOG ALL', whatever);
+  }
+
   render(){
+    console.log('Render State', this.state);
     return (
       <div className="App">
         <NumberHeader luckyNumber={this.state.number} />
+        <NumberForm defaultNumber={this.state.number}
+          saveNumber={this.updateNumber}
+          saveAll={this.logAll}
+          />
       </div>
     );
   }
@@ -23,6 +38,39 @@ function NumberHeader(props) {
   return (
     <h1 style={{color: 'green'}}>Your Lucky Number is {props.luckyNumber}</h1>
   )
+}
+
+class NumberForm extends React.Component{
+  constructor (props){
+    super(props);
+
+    this.state = {
+      inputNumber: props.defaultNumber || 0,
+    };
+  }
+
+  numberChange = e => {
+    this.setState({ inputNumber: e.target.value })
+    console.log(this.state);
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('SUBMIT!', this.state);
+    this.props.saveNumber(this.state.inputNumber);
+    this.props.saveAll(this.state);
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input value={this.state.inputNumber}
+          onChange={this.numberChange}
+          />
+        <button type="submit">Save</button>
+      </form>
+    )
+  }
 }
 
 export default App;
