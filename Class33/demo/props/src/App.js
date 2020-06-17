9e324e73d74b77e1fe9225be0afb8508ab8a3cfd
Keith Dahlby <keith@newbo.co>
2020-06-17 10:38:36 -0500

    Add NumberHeader

--- Class33/demo/props/src/App.js
+++ Class33/demo/props/src/App.js
@@ -3,8 +3,15 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      <NumberHeader luckyNumber={42} />
     </div>
   );
+}
+
+function NumberHeader(props) {
+  return (
+    <h1 style={{color: 'green'}}>Your Lucky Number is {props.luckyNumber}</h1>
+  )
 }
 
 export default App;
