d81dede07244743e2fe9448a1e88f18081c3a9ec
Keith Dahlby <keith@newbo.co>
2020-06-18 11:12:54 -0500

    Wire up some routes

--- Class34/demo/routing/src/components/app.js
+++ Class34/demo/routing/src/components/app.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+import Numbers from './numbers';
 import Users from './users';
 
-function App(){
+function Nav() {
   return (
-    <Users />
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/users">Users</Link></li>
+        <li><Link to="/numbers">Numbers</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Routing Demo</h1>
+        <Nav />
+      </header>
+      <Switch>
+        <Route exact path="/">
+          <h2>Welcome</h2>
+          <p>You might be interested in our <Link to="/users">Users</Link></p>
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/numbers">
+          <Numbers />
+        </Route>
+        <Route>
+          404
+        </Route>
+      </Switch>
+      <footer>
+        &copy; 2020
+      </footer>
+    </>
   );
 }
 
