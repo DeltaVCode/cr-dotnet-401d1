e2fde96e3d9c8cfd815d6412c66c4d2371a7049c
Keith Dahlby <keith@newbo.co>
2020-06-18 11:00:39 -0500

    Wrap App in BrowserRouter

--- Class34/demo/routing/src/index.js
+++ Class34/demo/routing/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
