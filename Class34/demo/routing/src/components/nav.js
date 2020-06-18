95a2adb911bfc7fcb2254958aef9488d3a163057
Keith Dahlby <keith@newbo.co>
2020-06-18 11:22:47 -0500

    Use NavLink

--- Class34/demo/routing/src/components/nav.js
+++ Class34/demo/routing/src/components/nav.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './nav.scss';
 
 export default function Nav() {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/users">Users</Link></li>
-        <li><Link to="/numbers">Numbers</Link></li>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/users">Users</NavLink></li>
+        <li><NavLink to="/numbers">Numbers</NavLink></li>
       </ul>
     </nav>
   );
