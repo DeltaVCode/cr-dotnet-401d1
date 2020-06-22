45fffe8079568abe760350450e635520c9f1d62c
Keith Dahlby <keith@newbo.co>
2020-06-22 11:37:21 -0500

    Demo useState

--- Class36/demo/src/components/hooks-counter.js
+++ Class36/demo/src/components/hooks-counter.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Counter(props) {
   // Destructuring allows specifying default value
+  // const initialCount = props.initialCount || 0;
   const { initialCount = 0 } = props;
 
-  let count = initialCount;
+  // let count = initialCount;
+  let [count, setCount] = useState(initialCount);
+
+  // this.state = { name: '' };
+  let [name, setName] = useState('');
+  // { value: 'Keith', setter: function }
+  console.log('Rendering HooksCounter', { count, name })
+
+  const updateName = e => setName(e.target.value);
 
   const increment = () => {
     // Doesn't work
-    count++;
+    // count++;
+    // console.log(count);
+
+    // like setState(count => count + 1)
+    setCount(count + 1);
   };
 
   return (
@@ -17,6 +30,10 @@ export default function Counter(props) {
       <button onClick={increment}>
         Update Counter
       </button>
+      <fieldset>
+        <input onChange={updateName} />
+        <div>Name: {name}</div>
+      </fieldset>
     </div>
   );
 }
