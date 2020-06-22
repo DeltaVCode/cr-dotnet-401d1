bbaba2fb8735527a80055be839197760f2c06664
Keith Dahlby <keith@newbo.co>
2020-06-22 11:55:04 -0500

    Show some useEffect

--- Class36/demo/src/components/hooks-counter.js
+++ Class36/demo/src/components/hooks-counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Counter(props) {
   // Destructuring allows specifying default value
@@ -11,7 +11,37 @@ export default function Counter(props) {
   // this.state = { name: '' };
   let [name, setName] = useState('');
   // { value: 'Keith', setter: function }
-  console.log('Rendering HooksCounter', { count, name })
+  // console.log('Rendering HooksCounter', { count, name })
+
+  // Only run once (no dependencies to trigger re-run)
+  useEffect(() => {
+    console.log('componentDidMount-ish');
+
+    // Function returned from useEffect is called at the end
+    // e.g. to cancel a fetch that didn't finish
+    return () => {
+      console.log('componentWillUnmount-ish');
+    };
+  }, []);
+
+  // Effect on every render, non-blocking!
+  useEffect(() => {
+    console.log('componentDidUpdate-ish');
+  });
+
+  // Only run when name changes
+  useEffect(() => {
+    if (name.length > 2)
+      console.log('Name changed to ' + name);
+  }, [name]);
+
+  // Only run when count changes
+  useEffect(() => {
+    console.log('Count changed')
+    setTimeout(() => {
+      document.title = 'Count: ' + count;
+    }, 500)
+  }, [count]);
 
   const updateName = e => setName(e.target.value);
 
