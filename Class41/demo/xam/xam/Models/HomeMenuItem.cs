7813bf25d5cb04db0c13fe170d59e40e8b825c64
Keith Dahlby <keith@newbo.co>
2020-06-29 22:27:43 -0500

    Empty Xamarin project

---
+++ Class41/demo/xam/xam/Models/HomeMenuItem.cs
@@ -0,0 +1,15 @@
+﻿namespace xam.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        Test,
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
